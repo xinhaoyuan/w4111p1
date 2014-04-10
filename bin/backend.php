068ecbb73905374064ed718fe7dd345d16a8aa2d
Xinhao Yuan <xinhaoyuan@gmail.com>
2014-04-10 02:52:21 -0400

    id data type changed

--- bin/backend.php
+++ bin/backend.php
@@ -128,12 +128,12 @@ class Backend extends DefaultIRest {
 
             $this->sql($conn,
                        "CREATE TABLE tbl_item (" .
-                       "iname VARCHAR(100) NOT NULL," .
-                       "item_id NUMBER(10)," .
-                       "idesc VARCHAR(1000)," .
-                       "price NUMBER(10, 2)," .
-                       "cname VARCHAR(100) NOT NULL," .
-                       "email VARCHAR(100) NOT NULL," .
+                       "iname     VARCHAR(100) NOT NULL," .
+                       "item_id   RAW(16) DEFAULT SYS_GUID()," .
+                       "idesc     VARCHAR(1000)," .
+                       "price     NUMBER(10, 2)," .
+                       "cname     VARCHAR(100) NOT NULL," .
+                       "email     VARCHAR(100) NOT NULL," .
                        "post_date DATE," .
                        "PRIMARY KEY (item_id)," .
                        "FOREIGN KEY (cname) REFERENCES tbl_catagory ON DELETE SET NULL," .
@@ -143,19 +143,19 @@ class Backend extends DefaultIRest {
             $this->sql($conn,
                        "CREATE TABLE tbl_photo (" .
                        "image_data BLOB," .
-                       "image_id NUMBER(10)," .
-                       "item_id NUMBER(10)," .
+                       "image_id   RAW(16) DEFAULT SYS_GUID()," .
+                       "item_id    RAW(16) NOT NULL," .
                        "PRIMARY KEY (image_id)," .
                        "FOREIGN KEY (item_id) REFERENCES tbl_item ON DELETE SET NULL" .
                        ")");
 
             $this->sql($conn,
                        "CREATE TABLE tbl_transaction (" .
-                       "trans_id NUMBER(10)," .
+                       "trans_id  RAW(16) DEFAULT SYS_GUID()," .
                        "last_date DATE," .
-                       "price NUMBER(10,2)," .
-                       "email VARCHAR(100) NOT NULL," .
-                       "item_id NUMBER(10) NOT NULL," .
+                       "price     NUMBER(10,2)," .
+                       "email     VARCHAR(100) NOT NULL," .
+                       "item_id   RAW(16) NOT NULL," .
                        "PRIMARY KEY (trans_id)," .
                        "FOREIGN KEY (email) REFERENCES tbl_user ON DELETE SET NULL," .
                        "FOREIGN KEY (item_id) REFERENCES tbl_item ON DELETE SET NULL" .
@@ -163,10 +163,10 @@ class Backend extends DefaultIRest {
 
             $this->sql($conn,
                        "CREATE TABLE tbl_message (" .
-                       "msg_id RAW(16) DEFAULT SYS_GUID()," .
+                       "msg_id    RAW(16) DEFAULT SYS_GUID()," .
                        "post_date DATE," .
-                       "content VARCHAR(1000)," .
-                       "trans_id NUMBER(10) NOT NULL," .
+                       "content   VARCHAR(1000)," .
+                       "trans_id  RAW(16) NOT NULL," .
                        "PRIMARY KEY (msg_id)," .
                        "FOREIGN KEY (trans_id) REFERENCES tbl_transaction ON DELETE SET NULL" .
                        ")");
@@ -212,72 +212,5 @@ class Backend extends DefaultIRest {
         return "Exception(" . $e->getMessage() . ")\n";
     }
 };
-
-class ItemManager extends DefaultIRest {
-
-    public static function instance() {
-        static $instance;
-        if (!($instance instanceof self))
-            $instance = new self();
-        return $instance; 
-    }
-
-    public function __construct() {
-    }
-
-    public function dispatch($path) {
-        if ($this->_parse_path($path, $name, $remain) === FALSE)
-            return NULL;
-        if ($name === "") {
-            return $this;
-        }
-
-        $item_id = $name;
-        return new ItemProxy($item_id);
-    }
-
-    public function post($args) {
-        /* post new items */
-        if (!SessionManager::instance()->authenticate_session($args))
-            return ["result" => "failed",
-                    "reason" => "authentication failed"];
-        
-        $iname = $args["iname"];
-        $idesc = $args["idesc"];
-        $price = (float)$args["price"];
-        $cname = $args["cname"];
-        $email = $args["email"];
-
-        $conn = Backend::instance()->get_db_conn();
-
-        try {
-            Backend::instance()->sql($conn,
-                                     "INSERT INTO tbl_item (iname, idesc, price, cname, email, post_date) " .
-                                     "VALUES ('$iname', '$idesc', '$price', '$cname', '$email', now());");
-        } catch (Exception $e) {
-            return array("result" => "failed",
-                         "reason" => "sql error");
-        }
-
-        /* creation success */
-        return array("result" => "success");
-    }
-
-    public function get($args) {
-        /* list all items */
-        if (!SessionManager::instance()->authenticate_session($args))
-            return array("result" => "failed",
-                         "reason" => "authentication failed");
-
-        $email = $args["email"];
-
-        $conn = Backend::instance()->get_db_conn();
-        $r = Backend::instance()->sql($conn,
-                                      "SELECT i.item_id FORM tbl_item i, tbl_user_group ug2, tbl_user_group ug " .
-                                      "WHERE ug.email = '$email' AND ug2.gname = ug.gname AND ug.email = i.email;");
-        return $r->fetch_row();
-    }
-};
-
 
 ?>
